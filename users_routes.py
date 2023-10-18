03fc9ec7ff6acddf657626c13fff3463b10e74bc
Michael Carey <mpcarey@csu.fullerton.edu>
2023-10-17 21:24:08 -0700

    implemented each endpoint

--- users_routes.py
+++ users_routes.py
@@ -2,8 +2,9 @@ import contextlib
 import sqlite3
 
 from typing import List
-from fastapi import Depends, HTTPException, APIRouter, status
-from users_schemas import User_info, Register
+from fastapi import Depends, HTTPException, APIRouter, status, Query
+from users_schemas import User_info, User
+from users_hash import hash_password, verify_password
 
 router = APIRouter()
 
@@ -35,7 +36,10 @@ def get_all_users(db: sqlite3.Connection = Depends(get_db)):
     for user in users_data:
         cursor.execute(
             """
-            SELECT role FROM role WHERE user_id = ?
+            SELECT role FROM user_role 
+            JOIN role ON user_role.role_id = role.rid
+            JOIN users ON user_role.user_id = users.uid
+            WHERE user_id = ?
             """,
             (user["uid"],)
         )
@@ -53,20 +57,97 @@ def get_all_users(db: sqlite3.Connection = Depends(get_db)):
 
     return users_info
 
-@router.get("/users/login", tags=['Users'])
-def get_user_login(db: sqlite3.Connection = Depends(get_db)):
-    #TODO 
-    # login enpoint implementation
+@router.post("/users/login", tags=['Users'])
+def get_user_login(user: User, db: sqlite3.Connection = Depends(get_db)):
     cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM users WHERE name = ?
+        """, (user.name,)
+    )
+    user_data = cursor.fetchone()
+    
+    # Check if user exists
+    if not user_data:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid username")
+
+    # Verify the password
+    if not verify_password(user.password, user_data['password']):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid password")
+    
+    #TODO: JWT validation
+
+    # return message
+    return {"message": "Login successful"}
 
 @router.post("/users/register", tags=['Users'])
-def register_new_user(register_data: Register, db: sqlite3.Connection = Depends(get_db)):
-    #TODO 
-    # register enpoint implementation
+def register_new_user(user: User, db: sqlite3.Connection = Depends(get_db)):
     cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM users WHERE name = ?
+        """, (user.name,)
+    )
+    user_data = cursor.fetchone()
+    
+    # Check if user exists
+    if user_data:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
 
-@router.get("/users/{uid}/password", tags=['Users'])
-def get_user_password(db: sqlite3.Connection = Depends(get_db)):
-    #TODO 
-    # /check user password enpoint implementation
+    # Hash the password before storing it
+    password_hash = hash_password(user.password)
+
+    #Store new user data in DB
+    cursor.execute(
+        """
+        INSERT INTO users (name, password)
+        VALUES (?, ?)
+        """, (user.name, password_hash)
+    )
+
+    #Give new user default role of 'student'
+    cursor.execute(
+        """
+        SELECT * FROM users WHERE name = ?
+        """, (user.name,)
+    )
+    user_data = cursor.fetchone()
+
+    cursor.execute(
+        """
+        INSERT INTO user_role (user_id, role_id)
+        VALUES (?, ?)
+        """, (user_data['uid'], 1)
+    )
+
+    db.commit()
+
+    return {"message": "User created successfully"}
+
+@router.get("/users/check_password", tags=['Users'])
+def get_user_password(username: str = Query(..., title="Username", description="Your username"),
+    password: str = Query(..., title="Password", description="Your password"),
+    db: sqlite3.Connection = Depends(get_db)):
+
+    # Query the database to retrieve the user's password hash
     cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT password FROM users WHERE name = ?
+        """, (username,)
+    )
+    q = cursor.fetchone()
+
+    # Check if user exists
+    if not q:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    # Check the password
+    password_hash = q[0]
+
+    if verify_password(password, password_hash):
+        return {"message": "Password is correct"}
+    else:
+        return {"message": "Password is incorrect"}
+    
+
